f3a7a5e0efa9892928c2a4fd5c4082fa7e0ae3a1
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2024-05-11 21:34:38 +0300

    Create Employee.cs

---
+++ Intermediate/Methods/Employee.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Employee
+    {
+        // Static and Constant are stored in the place called High Frequancy Heap
+        public static double TAX = 0.03;
+        public string Fname;
+        public string Lname;
+        public double Wage;
+        public double LoggedHours;
+    }
+}
