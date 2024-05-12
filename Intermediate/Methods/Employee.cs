86d2977776adc8d4ac22dc8a9baa348572aa88cc
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2024-05-12 18:48:55 +0300

    Update Employee.cs
    added some methods and make refactor

--- Intermediate/Methods/Employee.cs
+++ Intermediate/Methods/Employee.cs
@@ -13,5 +13,24 @@ namespace Methods
         public string Lname;
         public double Wage;
         public double LoggedHours;
+
+        // Refactor salary slip application
+
+        // All these method we don't use it in the main so we will make it encapsulation
+        private double Calculate() => Wage * LoggedHours;
+        private double CalculateTax() => Calculate() * TAX;
+        private double CalculateNet() => Calculate() - CalculateTax();
+
+        public string PrintSlip()
+        {
+            return $"\nFirst Name: {Fname}" + 
+            $"\nLast Name: {Lname}" + 
+            $"\nWage: {Wage}" + 
+            $"\nLoggedHours: {LoggedHours}" + 
+            "\n----------------------------" + 
+            $"\nSalary: ${Calculate()}" + 
+            $"\nDeductble Tax ({TAX * 100}%) Amount: ${CalculateTax()}" +
+            $"\nnetSalary: {CalculateNet()}\n";
+        }
     }
 }
